aca4c8020a329eda20170cdc9c235d87129bb1b1
linyos <laso85143@gmail.com>
2023-11-26 18:08:30 +0800

    Build Producer

---
+++ RabbitMQTEST/RabbitMQTEST/Model/UserInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQTEST.Model
+{
+    /// <summary>
+    /// User Info Basic (protected)
+    /// </summary>
+    public class UserInfo
+    {
+        protected string userName = "guest";
+
+        protected string password = "guest";
+
+        protected string hostName = "localhost";
+    }
+
+    /// <summary>
+    /// User Info
+    /// </summary>
+    public class MyUserInfo : UserInfo
+    {
+        public string UserName { get { return userName; } }
+
+        public string PassWord { get { return password; } }
+
+        public string HostName { get { return hostName; } }
+    }
+}
--- RabbitMQTEST/RabbitMQTEST/Program.cs
+++ RabbitMQTEST/RabbitMQTEST/Program.cs
@@ -3,13 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQTEST.Model;
 
 namespace RabbitMQTEST
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static MyUserInfo _userInfo = new MyUserInfo();
+
+        public static void Main(string[] args)
         {
+            string queueName = "MyQueue";
+            string exchangeName = "MyExchange";
+            string routingKeyName = "MyroutingKey";
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                UserName = _userInfo.UserName,
+                Password = _userInfo.PassWord,
+                HostName = _userInfo.HostName
+            };
+
+            using (var connection = factory.CreateConnection())
+
+            using (var channel = connection.CreateModel())
+            {
+                // 建立Queue
+                channel.QueueDeclare(queue: queueName,
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+
+                // 建立 exChange
+                channel.ExchangeDeclare(exchange: exchangeName,
+                                        durable: false,
+                                        autoDelete: false,
+                                        arguments: null,
+                                        type: ExchangeType.Direct);
+
+                // 綁定 Queue跟 exchange
+                channel.QueueBind(queueName, exchangeName, routingKeyName);
+
+                Console.WriteLine("\n RabbitMQ連接成功");
+
+                string input = string.Empty;
+                do
+                {
+                    input = Console.ReadLine();
+                    var sendBytes = Encoding.UTF8.GetBytes(input);
+                    // 發布訊息到RabbitMQ 到Service
+                    channel.BasicPublish(exchangeName, routingKeyName, null, sendBytes);
+                }
+                while (Console.ReadKey().Key != ConsoleKey.Escape);
+            }
         }
     }
 }
